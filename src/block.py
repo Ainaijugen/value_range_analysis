976cde34a8ff653b6e13307f21f3723a8a4064db
penguinnnnn <HRZpenguin@gmail.com>
2018-06-12 02:03:53 +0800

    add analysis of pred and succ

--- src/block.py
+++ src/block.py
@@ -1,19 +1,19 @@
 class Block:
-    def __init__(self, list):
-        list = list.splitlines()
+    def __init__(self, List):
+        List = List.splitlines()
         self.stat = []
-        if list[0][0] != '<':
+        if List[0][0] != '<':
             self.id = '<bb 1>'
-            list[0] = list[0][list[0].find('(') + 1:list[0].find(')')]
-            list[0] = list[0].split(',')
-            for s in list[0]:
+            List[0] = List[0][List[0].find('(') + 1:List[0].find(')')]
+            List[0] = List[0].split(',')
+            for s in List[0]:
                 self.stat.append(s)
-            for line in list[1:]:
+            for line in List[1:]:
                 if line[0] != '{':
                     self.stat.append(line[:line.find(';')])
         else:
-            self.id = list[0][:list[0].find(':')]
-            for line in list[1:]:
+            self.id = List[0][:List[0].find(':')]
+            for line in List[1:]:
                 if line[0] != '}':
                     if line[-1] == ';':
                         self.stat.append(line[:-1])
--- src/function.py
+++ src/function.py
@@ -1,28 +1,49 @@
 from block import Block
 
 class Function:
-    def __init__(self, list):
-        list = list.splitlines()
+    def __init__(self, List):
+        List = List.splitlines()
         tmp_list = []
-        for line in list:
+        for line in List:
             if line.strip() != '':
                 tmp_list.append(line.lstrip())
         tmp_list = tmp_list[1:]
         for i in range(len(tmp_list)):
             if tmp_list[i][0] == '<':
                 tmp_list[i] = '@@' + tmp_list[i]
-        list = '\n'.join(tmp_list)
+        List = '\n'.join(tmp_list)
         
-        self.name = list[:list.find('(') - 1]
+        self.name = List[:List.find('(') - 1]
         print('Function: ' + self.name + ' detected, Creating basic blocks:')
         
-        list = list.split('@@')
+        List = List.split('@@')
         self.blocklist = {}
-        for blocks in list:
+        for blocks in List:
             block_obj = Block(blocks)
             self.blocklist[block_obj.id] = block_obj
 
+        #Analysis of pred and succ
+        print('Basic blocks created, analyze pred and succ:')
+        for bb in self.blocklist:
+            for stats in self.blocklist[bb].stat:
+                if stats[0:4] == 'goto':
+                    self.blocklist[bb].succ.append(stats[5:])
+                    self.blocklist[stats[5:]].pred.append(self.blocklist[bb].id)
+            for i in range(len(List)):
+                if i == 0:
+                    self.blocklist['<bb 1>'].succ.append(self.blocklist[List[1][:List[1].find('>') + 1]].id)
+                elif List[i][:List[i].find('>') + 1] == self.blocklist[bb].id:
+                    if i + 1 < len(List):
+                        self.blocklist[bb].succ.append(self.blocklist[List[i + 1][:List[i + 1].find('>') + 1]].id)
+                    if i - 1 > 0:
+                        self.blocklist[bb].pred.append(self.blocklist[List[i - 1][:List[i - 1].find('>') + 1]].id)
+                    if i == 1:
+                        self.blocklist[bb].pred.append('<bb 1>')
+
+        for bb in self.blocklist:
+            self.blocklist[bb].pred = list(set(self.blocklist[bb].pred))
+            self.blocklist[bb].succ = list(set(self.blocklist[bb].succ))
+            print(self.blocklist[bb].id, self.blocklist[bb].pred, self.blocklist[bb].succ)
+
     def calculate(self, _IN):
-        temp = ()
-        
         return
